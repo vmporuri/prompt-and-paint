ed47541d1084dbfda894a2277b1a87b8d537ce74
Venu Poruri <vmporuri@gmail.com>
2024-07-14 18:22:31 -0500

    fix: leaking game events to client

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 # To-Do List
 
-- Fix event type leakage to client
 - Fix game stalling when user leaves
+- Separate dev and deployment docker setups
 - Update documentation
 - Update README.md
--- internal/game/templates.go
+++ internal/game/templates.go
@@ -2,15 +2,12 @@ package game
 
 import (
 	"bytes"
-	"fmt"
 	"html/template"
 	"path/filepath"
 )
 
-func generateTemplate(buf *bytes.Buffer, path string, data any) ([]byte, error) {
-	if buf == nil {
-		buf = &bytes.Buffer{}
-	}
+func generateTemplate(path string, data any) ([]byte, error) {
+	buf := &bytes.Buffer{}
 	tmpl, err := template.ParseFiles(path)
 	if err != nil {
 		return nil, err
@@ -23,37 +20,29 @@ func generateTemplate(buf *bytes.Buffer, path string, data any) ([]byte, error) 
 }
 
 func generateUsername() ([]byte, error) {
-	return generateTemplate(nil, filepath.Join("templates", "username.html"), nil)
+	return generateTemplate(filepath.Join("templates", "username.html"), nil)
 }
 
 func generateWaitingPage(wpd *waitingPageData) ([]byte, error) {
-	return generateTemplate(nil, filepath.Join("templates", "waiting-page.html"), wpd)
+	return generateTemplate(filepath.Join("templates", "waiting-page.html"), wpd)
 }
 
 func generatePlayerList(pld *playerListData) ([]byte, error) {
-	buf := &bytes.Buffer{}
-	buf.Write([]byte(fmt.Sprintf("%s:", newPlayerList)))
-	return generateTemplate(buf, filepath.Join("templates", "player-list.html"), pld)
+	return generateTemplate(filepath.Join("templates", "player-list.html"), pld)
 }
 
 func generateGamePage(gpd *gamePageData) ([]byte, error) {
-	buf := &bytes.Buffer{}
-	buf.Write([]byte(fmt.Sprintf("%s:", enterGame)))
-	return generateTemplate(buf, filepath.Join("templates", "game-page.html"), gpd)
+	return generateTemplate(filepath.Join("templates", "game-page.html"), gpd)
 }
 
 func generateVotingPage(vpd *votingPageData) ([]byte, error) {
-	buf := &bytes.Buffer{}
-	buf.Write([]byte(fmt.Sprintf("%s:", votePage)))
-	return generateTemplate(buf, filepath.Join("templates", "voting-page.html"), vpd)
+	return generateTemplate(filepath.Join("templates", "voting-page.html"), vpd)
 }
 
 func generatePicturePreview(ipd *imagePreviewData) ([]byte, error) {
-	return generateTemplate(nil, filepath.Join("templates", "image-preview.html"), ipd)
+	return generateTemplate(filepath.Join("templates", "image-preview.html"), ipd)
 }
 
 func generateLeaderboardPage(lpd *leaderboardPageData) ([]byte, error) {
-	buf := &bytes.Buffer{}
-	buf.Write([]byte(fmt.Sprintf("%s:", sendLeaderboard)))
-	return generateTemplate(buf, filepath.Join("templates", "leaderboard.html"), lpd)
+	return generateTemplate(filepath.Join("templates", "leaderboard.html"), lpd)
 }
